1de925724495955c5e8e071e35e8c597a2b43a44
Joy <sahajoydeep2002@gmail.com>
2024-12-06 05:34:10 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Joy's Website</h2>
+      <p>My name is Joydeep saha, I am computer science student at BITS-Pilani, and I am recording this video to demonstrate
+        how to deploy react webpage to a webserver for free
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
